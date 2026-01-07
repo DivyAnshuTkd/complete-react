93e4f4454b59a1d2d37e789bd7bb24d5aefe0d21
DivyAnshuTkd <divyanshu2907tkd@gmail.com>
2026-01-08 02:18:11 +0530

    context api discussion with project.

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -3,3 +3,9 @@ import React from 'react'
 const UserContext = React.createContext()   // React.createContext() is a method that creates a Context object.
 
 export default UserContext;
+
+
+
+
+
+// Creating a UserContext to manage user-related data across components without prop drilling.
